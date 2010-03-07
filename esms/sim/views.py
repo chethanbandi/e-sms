17e122e8e944e29206ecf4ceb936c38d676d5df2
chethanbandi <chethanbandi@e79e0db6-1b00-11df-8134-2392cdf085bb>
2010-03-07 03:53:24 +0000

    Added a sample menu for SIM

--- esms/sim/views.py
+++ esms/sim/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import render_to_response
 from django.template import loader, Context
 
 def sim_home(request):
+    """
     t = loader.get_template('sim/index.html')
     c = Context()
-    return HttpResponse(t.render(c))
+    data = serializers.serialize("json", t.render(c))
+    return HttpResponse(data, mimetype="application/javascript")
+    #return HttpResponse(t.render(c))
     #return HttpResponse(t)
+    """
+    return render_to_response("sim/index.html", {})
 
