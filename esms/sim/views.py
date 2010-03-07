1f29c03c2eb8a76fa36ae1f87ad0e8ba38d8871f
chethanbandi <chethanbandi@e79e0db6-1b00-11df-8134-2392cdf085bb>
2010-03-07 01:33:16 +0000

    sim home page templates and view is created

--- esms/sim/views.py
+++ esms/sim/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.template import loader, Context
 
 def sim_home(request):
     t = loader.get_template('sim/index.html')
-    return HttpResponse(t)
+    c = Context()
+    return HttpResponse(t.render(c))
+    #return HttpResponse(t)
 
